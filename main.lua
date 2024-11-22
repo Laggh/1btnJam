ae955bf4354830038ddd6914b6ae7a8012132cb2
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2024-11-22 20:38:34 -0300

    more refactoring
    bruh

--- main.lua
+++ main.lua
@@ -3,14 +3,32 @@ config = require("config")
 loadedFiles = require("fileLoader")
 img = loadedFiles.img
 sfx = loadedFiles.sfx
+
+
+function checkInput()
+    if input then inputLenght = inputLenght + 1
+    else if inputLenght > 0 then
+        table.insert(inputArr, inputLenght)
+        inputLenght = 0
+    end
+        noInputLength = noInputLength + 1
+    end
+    if noInputLength > 60 then
+        inputArr = {}
+        noInputLength = 0
+    end
+end
+
+function characterToMorseCode(_Character)
+    return constants.morseCodeTable[_Character]
+end
+
 function morseCodeToCharacter(_MorseCode)
     for i, v in pairs(constants.morseCodeTable) do
-        if v == _MorseCode then
-            return i
-        end
+        if v == _MorseCode then return i end
     end
-    return nil
 end
+
 
 function arrToMorseCode(_Arr)
     local morseCode = ""
@@ -28,33 +46,21 @@ function love.keypressed(_Key) input = true end
 function love.keyreleased(_Key) input = false end
 
 function love.load()
-    --input is an boolean that is set to true when there is input
     input = false
     inputLenght = 0
     noInputLength = 0
     inputArr = {}
+end
 
+function love.update(dt)
+    checkInput()
 
 end
 
-function love.update()
-    if input then
-        inputLenght = inputLenght + 1
-    else
-        if inputLenght > 0 then
-            table.insert(inputArr, inputLenght)
-            inputLenght = 0
-        end
-        noInputLength = noInputLength + 1
-    end
-    if noInputLength > 60 then
-        inputArr = {}
-        noInputLength = 0
-    end
-end
-
-function love.draw()
-    love.graphics.print(arrToMorseCode(inputArr), 0, 0)
+function love.draw(dt)
+    local morseCode = arrToMorseCode(inputArr)
+    love.graphics.print(morseCode, 0, 0)
+    love.graphics.print(morseCodeToCharacter(morseCode), 0, 10)
     for i, v in ipairs(inputArr) do
         love.graphics.print(v, 0, 20+(i * 20))
     end
